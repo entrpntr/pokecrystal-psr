867b96f9533f45386fb547f1f19e878dcb434412
entrpntr <entrpntr@gmail.com>
2021-10-18 11:52:06 -0400

    Change location of savs; increase max number of savs to 68.

--- engine/rtc/reset_password.asm
+++ engine/rtc/reset_password.asm
@@ -20,16 +20,14 @@ _ResetClock::
 	cp -1
 	ret z
 	push bc
-	ld b, a
-	ld hl, $4000
-	and $3
-	swap a
-	add h
-	ld h, a
-	ld a, b
-	srl a
-	srl a
-	add $78 ; first bank with save data
+	ld b, 0
+	ld c, a
+	ld hl, .FreeSpace
+	add hl, bc
+	add hl, bc
+	ld a, [hli]
+	ld h, [hl]
+	ld l, 0
 	call XferSave
 	pop bc
 	ld a, b
@@ -151,13 +149,16 @@ _ResetClock::
 	dba NULL
 	dba NULL
 
+MAX_SAVS EQU 68
+SAV_NAME_LENGTH EQU 17
+
 .Name
 	ld a, [wMenuSelection]
 	cp -1
 	ret z
 	push de
 	ld hl, .Strings
-	ld bc, $11
+	ld bc, SAV_NAME_LENGTH
 	call AddNTimes
 	ld d, h
 	ld e, l
@@ -166,10 +167,92 @@ _ResetClock::
 
 ; update .Numbers and .Strings using custom tool along with .sav files
 .Numbers
-	db 0
-	db -1
-	db 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-	db -1
+	db  0 ; num savs
+.ids
+	table_width 1, .ids
+	db -1,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15
+	db 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+	db 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47
+	db 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63
+	db 64, 65, 66, 67
+	assert_table_length MAX_SAVS
+	db -1 ; end
 
 .Strings
-	ds 16 * $11
+	table_width SAV_NAME_LENGTH, .Strings
+REPT MAX_SAVS
+	db "                @"
+ENDR
+	assert_table_length MAX_SAVS
+
+.FreeSpace
+	table_width 2, .FreeSpace
+	db $0b, $70
+	db $11, $50
+	db $11, $60
+	db $11, $70
+	db $12, $70
+	db $20, $70
+	db $21, $70
+	db $28, $60
+	db $28, $70
+	db $29, $70
+	db $2c, $60
+	db $2c, $70
+	db $2e, $60
+	db $2e, $70
+	db $2f, $70
+	db $35, $70
+	db $36, $60
+	db $36, $70
+	db $3f, $60
+	db $3f, $70
+	db $41, $70
+	db $59, $70
+	db $5a, $70
+	db $5b, $60
+	db $5b, $70
+	db $61, $70
+	db $6b, $70
+	db $6c, $70
+	db $6e, $60
+	db $6e, $70
+	db $6f, $70
+	db $70, $60
+	db $70, $70
+	db $71, $70
+	db $72, $70
+	db $73, $60
+	db $73, $70
+	db $74, $60
+	db $74, $70
+	db $75, $40
+	db $75, $50
+	db $75, $60
+	db $75, $70
+	db $76, $40
+	db $76, $50
+	db $76, $60
+	db $76, $70
+	db $78, $50
+	db $78, $60
+	db $78, $70
+	db $79, $40
+	db $79, $50
+	db $79, $60
+	db $79, $70
+	db $7a, $40
+	db $7a, $50
+	db $7a, $60
+	db $7a, $70
+	db $7b, $50
+	db $7b, $60
+	db $7b, $70
+	db $7c, $50
+	db $7c, $60
+	db $7c, $70
+	db $7d, $70
+	db $7f, $40
+	db $7f, $50
+	db $7f, $60
+	assert_table_length MAX_SAVS
